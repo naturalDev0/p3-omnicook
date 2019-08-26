b4634906cdbcea5315829b12dcd04a7683c2af3b
Tommy Lee <tonglin.lee@gmail.com>
2019-08-26 06:16:15 +0000

    1. Added add, edit, delete and view routes for later use. 2. Added comments into app.py. 3. Made some frontend changes to 'add' and 'edit' html pages.

--- app.py
+++ app.py
@@ -12,25 +12,31 @@ app = Flask(__name__)
 #     database="Chinook" #the name of database we want to use
 # )
 
+# ROUTE : Homepage
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# ROUTE : Add recipe
 @app.route('/recipe/add')
 def add_recipe():
     return render_template("recipe_add.html")
 
+# ROUTE : Edit recipe
 @app.route('/recipe/edit')
 def edit_recipe():
     return render_template("recipe_edit.html")
-    
+
+# ROUTE : View recipe    
 @app.route('/recipe')
 def view_recipe():
     return render_template("recipe_view.html")
-    
+
+# ROUTE : Delete recipe    
 @app.route('/recipe/delete')
 def delete_recipe():
     return render_template("recipe_delete.html")
+
 
 # Boilerplate
 if __name__ == '__main__':
